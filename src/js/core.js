b76c9206d86b038f6d0295b0c648efdbd8244d77
longjia <longjiarun@qq.com>
2015-10-18 18:07:44 +0800

    fix core.os bug

--- src/js/core.js
+++ src/js/core.js
@@ -34,21 +34,32 @@
     var toString = Object.prototype.toString;
 
     ;(function(){
-        var proto = this,ua = navigator.userAgent,os;
+        var proto = this,ua = navigator.userAgent;
         this.constructor = Core;
 
-        
         var android = ua.match(/(Android);?[\s\/]+([\d.]+)?/),
             ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
             ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/),
             iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
             wp = ua.match(/Windows Phone ([\d.]+)/),
             os = {};
-        if (android) os.android = true, os.version = android[2];
-        if (iphone && !ipod) os.ios = os.iphone = true, os.version = iphone[2].replace(/_/g, '.')
-        if (ipad) os.ios = os.ipad = true, os.version = ipad[2].replace(/_/g, '.')
-        if (ipod) os.ios = os.ipod = true, os.version = ipod[3] ? ipod[3].replace(/_/g, '.') : null;
-        if (wp) os.wp = true, os.version = wp[1];
+
+        //android
+        android ? (os.android = true, os.version = android[2]) : (os.android = false);
+
+        //iphone
+        iphone && !ipod ? ( os.ios = os.iphone = true, os.version = iphone[2].replace(/_/g, '.') ) : (os.iphone  = false);
+
+        //ipad
+        ipad ? ( os.ios = os.ipad = true, os.version = ipad[2].replace(/_/g, '.') ) : (os.ipad  = false);
+        
+        //ipod
+        ipod ? ( os.ios = os.ipod = true, os.version = ipod[3].replace(/_/g, '.') ) : (os.ipod = false);
+
+        //window phone
+        wp ? ( os.wp = true, os.version = wp[1]) : (os.wp = false);
+
+        !os.iphone && !os.ipad && !os.ipod && (os.ios = false);
 
         os.phone = os.android && /mobile/i.test(ua) || os.iphone || os.wp ? true : false;
         os.tablet = !os.phone && ( os.android || os.ipad || /window/i.test(ua) && /touch/i.test(ua) ) ? true : false;
