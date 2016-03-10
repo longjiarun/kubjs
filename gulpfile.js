f5652e26688f592940753e747aa507959380d5d0
longjia <longjiarun@qq.com>
2016-03-10 11:25:01 +0800

    kubjs 版本2

--- gulpfile.js
+++ gulpfile.js
@@ -2,102 +2,54 @@ var gulp = require("gulp"),
     less = require("gulp-less"),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css'),
-    rename = require("gulp-rename"),
     del = require('del');
 
-var src = "src", build = "build";
+var src = "src",
+    build = "build";
 
 //clean
-gulp.task("clean",function(){
+gulp.task("clean", function() {
     del.sync(build);
 });
 
-//copy
-gulp.task("copy",["clean"],function(){
-    return gulp.src([src+"/**/*"])
-        .pipe(gulp.dest(build)); 
-});
-
-gulp.task("css",function(){
-    return gulp.src([src+"/**/*.less"])
+gulp.task("css", function() {
+    return gulp.src([src + "/**/*.less"])
         .pipe(less())
         .pipe(gulp.dest(build))
-        .pipe(minifyCss())
-        .on("end",function(){
-            
+        .on("end", function() {
+
         })
-        .on("error",function(err){
+        .on("error", function(err) {
             err && console.log(err.message);
         });
 });
 
-gulp.task("js",function(){
-    return gulp.src([src+"/**/*.js"])
-        //.pipe(uglify())
+gulp.task("js", function() {
+    return gulp.src([src + "/**/*.js"])
         .pipe(gulp.dest(build))
-        .on("end",function(){
-            
+        .on("end", function() {
+
         })
-        .on("error",function(err){
+        .on("error", function(err) {
             err && console.log(err.message);
         });
 });
 
-gulp.task("doc",function(){
-    gulp.src(build+"/**.js")
-      .pipe(jsdoc("docs"))
+gulp.task("default", ["clean"], function() {
+    gulp.start("css", "js");
 });
 
-var markdox = require("gulp-markdox");
-
-//只生成文档
-//docker -i src/js -o docs/html/kub -x lib 
-
-//监听文档并生成文档
-//docker -i src/js -o docs/html/kub -x lib -w
-gulp.task("doc", function(){
-    del("docs/markdown",function(){
-        gulp.src([build+"/js/**/*.js","!/js/lib/*.js"])
-            .pipe(markdox())
-            .pipe(rename(function(path){
-                path.extname = ".md";
-            }))
-            .pipe(gulp.dest("docs/markdown"));
-    });
-});
-
-//发布文档
-var publish = require("./gulp.publish"),
-    apiPath = "docs/html";
-publish({
-    build:apiPath,
-    rootDir : 'api'
-});
-
-//运行 publishdoc 发布文档
-gulp.task("publishdoc",function(){
-    return gulp.src(apiPath+"/kub/core.js.html")
-        .pipe(rename(function(path){
-            path.basename = "index"
-        }))
-        .pipe(gulp.dest(function(file){
-            file.contents = new Buffer("<script>window.location.href ='core.js.html'; </script>");
-            return apiPath+"/kub";
-        }))
-        .on("end",function(){
-            gulp.start("publish");
-        });
-});
-
-gulp.task("default",["clean"],function(){
-    gulp.start("css","js");
-});
-
-gulp.task("watch",["default"],function(){
-    gulp.watch([src+"/**/*.less"],function(){
+gulp.task("watch", ["default"], function() {
+    gulp.watch([src + "/**/*.less"], function() {
         gulp.start("css")
     });
-    gulp.watch([src+"/**/*.js"],function(){
+    gulp.watch([src + "/**/*.js"], function() {
         gulp.start("js");
     });
 });
+
+//只生成文档
+//docker -i src -o docs -x lib
+
+//监听文档并生成文档
+//docker -i src -o docs -x lib -w
--- src/js/extend/fn.js
+++ src/js/extend/fn.js
@@ -7,12 +7,12 @@
 !function(root){
     var $ = root.jQuery||root.Zepto,
         $body = $(document.body);
-    
+
     /**
      * ## $.fn.focuson
-     * 
+     *
      * 解决iPhone下同时进行错误提示与focus，会出现错误提示框出不来。
-     * 
+     *
      * 错误提示框被隐藏下键盘下。是由于iPhone对页面适配做出的处理，iPhone并没有将这个页面进行滚动处理。
      *
      * @return {} [description]
@@ -28,7 +28,7 @@
 
     /**
      * ## $.fn.isHidden
-     * 
+     *
      * 判断元素是否隐藏
      * @return {Boolean} true：隐藏，false：显示
      */
@@ -39,12 +39,12 @@
 
     /**
      * ## $.fn.isVisible
-     * 
+     *
      * 判断元素是否显示
      * @return {Boolean} true：显示，false：隐藏
      */
     $.fn.isVisible = function(){
         return ! this.isHidden();
     }
-    
-}(this);
+
+}(window);
